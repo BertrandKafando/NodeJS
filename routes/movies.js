05ff6877e81f47a150f4f9c43096bf70ae5c5815
bertrand <bertrandkafando07@gmail.com>
2022-01-21 13:48:32 +0100

    version 1.1

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var movies = require('../movies_reduit.json');
-var router = express.Router();
-router.get('/' ,(req,res,next)=>{
-  
-    res.json(movies);
-})
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+ 
+
+router.get('/', async function(req, res, next) {
+    const movies = await prisma.movies.findMany({take: 10})
+  res.send(movies);
+});
 
 
 
-module.exports=router;
+module.exports = router;
